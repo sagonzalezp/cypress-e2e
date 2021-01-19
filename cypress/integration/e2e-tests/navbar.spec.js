bd0803c59cbb8816b4a08ab4304b555e938e0e09
sagonzalezp <ser55.rock@gmail.com>
2021-01-18 22:23:23 -0500

    49. E2E Test: Navbar Links

---
+++ cypress/integration/e2e-tests/navbar.spec.js
@@ -0,0 +1,24 @@
+/// <reference types="Cypress" />
+describe('Navbar test', ()=>{
+    before(function(){
+        cy.visit('http://zero.webappsecurity.com/index.html')
+    })
+
+    it('should display online banking content', ()=>{
+        cy.get('#onlineBankingMenu').click()
+        cy.url().should('include', 'online-banking.html')
+        cy.get('h1').should('be.visible')
+        cy.get('h1').should('have.text', 'Online Banking')
+    }) 
+
+    it('should display feedback content', ()=>{
+        cy.get('#feedback > div').click()
+        cy.url().should('include', 'feedback.html')
+        cy.get('#feedback-title').should('have.text', 'Feedback')
+    })
+
+    it('should display homepage content', ()=>{
+        cy.get('.brand').click()
+
+    })
+})
